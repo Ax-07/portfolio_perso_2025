621039bcea0302617e06765e1531ae975c600b9a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-09-07 11:45:25 +0200

    Refactor le style des éléments de navigation pour améliorer la lisibilité et la cohérence visuelle

--- src/components/layouts/desktop-navigation.tsx
+++ src/components/layouts/desktop-navigation.tsx
@@ -42,12 +42,12 @@ export const DesktopNavigation = () => {
               return (
                 <NavigationMenuItem key={item.name}>
                     <Link href={item.href} onClick={(e) => handleNavClick(e, item.href)} className={cn(
-                      "group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium hover:bg-primary/30 hover:text-primary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1",
-                      isActive && "bg-primary/20 text-primary font-medium"
+                      "group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium hover:bg-primary/30 disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1",
+                      isActive && "bg-primary/20 font-medium"
                     )}>
                       <div className="flex items-center gap-2">
                         <IconComponent
-                          className={cn("h-4 w-4 transition-colors", isActive ? "text-primary" : "hover:text-primary")}
+                          className={cn("h-4 w-4 transition-colors")}
                         />
                         {item.name}
                       </div>

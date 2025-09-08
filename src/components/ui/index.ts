85c54925df7062210cf0a908c05ccfe9c77e73ff
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-09-08 23:58:37 +0200

    Ajout du fichier d'index pour l'export des composants UI spécifiques aux projets

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+// Export des composants UI spécifiques aux projets
+export { ProjectStatusBadge } from './project-status-badge';
+export { TechBadge } from './tech-badge';
+export { ProjectImage } from './project-image';
+export { TechList } from './tech-list';

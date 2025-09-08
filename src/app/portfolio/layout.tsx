61d33074caf90aded7da7f287f152f6db3b8ad54
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-09-08 23:58:02 +0200

    Ajout du composant Layout avec la configuration des métadonnées pour le portfolio

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/layouts/header";
+import { SEO_CONFIG } from "@/config";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: SEO_CONFIG.pages.portfolio.description,
+  openGraph: {
+    title: `Portfolio | ${SEO_CONFIG.openGraph.siteName}`,
+    description: SEO_CONFIG.pages.portfolio.description,
+    url: `${SEO_CONFIG.canonical}/portfolio`,
+  },
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}

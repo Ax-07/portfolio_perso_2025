9efa31c6c65e98caac9ac8d4ffe4c6fcffe21866
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-09-11 10:29:39 +0200

    Ajout du composant Header dans la page Portfolio et suppression de la métadonnée SEO.

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,17 +1,11 @@
-import { Metadata } from "next";
+import { Header } from "@/components/layouts/header";
 import { PortfolioPage } from "@/components/pages";
-import { SEO_CONFIG } from "@/config";
-
-export const metadata: Metadata = {
-  title: "Portfolio",
-  description: SEO_CONFIG.pages.portfolio.description,
-  openGraph: {
-    title: `Portfolio | ${SEO_CONFIG.openGraph.siteName}`,
-    description: SEO_CONFIG.pages.portfolio.description,
-    url: `${SEO_CONFIG.canonical}/portfolio`,
-  },
-};
 
 export default function Portfolio() {
-  return <PortfolioPage />;
+  return (
+    <>
+      <Header/>
+      <PortfolioPage />
+    </>
+  );
 }

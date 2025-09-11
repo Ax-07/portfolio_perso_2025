374b454bf7556ac35e81494920a33c9bb124f507
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-09-11 10:29:31 +0200

    Suppression de l'importation du composant Footer dans le fichier page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { ProjectsSection } from "@/components/sections/projects-section";
 import { HeroSection } from "@/components/sections/hero-section";
 import { Separator } from "@/components/ui/separator";
 import { ContactSection } from "@/components/sections/contact-section";
-import { Footer } from "@/components/layouts/footer";
 import { Header } from "@/components/layouts/header";
 import { SmoothScrollProvider } from "@/components/providers/smooth-scroll-provider";
 import { ResponsiveNavigation } from "@/components/layouts/responsive-navigation";

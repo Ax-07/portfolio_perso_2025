febde8461dd4393e68ca7b7b8fe94dd2faa10409
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-01-11 14:11:15 +0100

    Ajout de la classe z-50 et mise à jour des positions du bouton "Haut de page" pour améliorer la visibilité et l'accessibilité.

--- src/components/customs/ScrollToTopBtn.tsx
+++ src/components/customs/ScrollToTopBtn.tsx
@@ -7,7 +7,7 @@ import { scrollToTop } from '@/lib/utils';
 
 const ScrollToTopBtn = () => {
     return (
-      <Button variant="outline" size="sm" className="fixed bottom-4 right-4" onClick={() => scrollToTop()}>
+      <Button variant="outline" size="sm" className="fixed z-50 bottom-16 right-16" onClick={() => scrollToTop()}>
           <ArrowUp className="h-4 w-4 mr-2" />
           Haut de page
       </Button>

532faafcace8a2a41a5e630c6433ba85a4a68abb
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-01-12 11:13:25 +0100

    Suppression de la constante de taille pour simplifier la gestion des dimensions de l'image générée.

--- src/app/og-image/route.tsx
+++ src/app/og-image/route.tsx
@@ -2,12 +2,6 @@ import { ImageResponse } from "next/og";
 import { SITE_METADATA } from "@/config";
 
 export const runtime = "edge";
-
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
 export const contentType = "image/png";
 
 export async function GET() {
@@ -221,7 +215,8 @@ export async function GET() {
       </div>
     ),
     {
-      ...size,
+      width: 1200,
+      height: 630,
     }
   );
 }
